c0b0f908db586da0f5c42d92966d5a42ebf0a666
Igor Sinigur <fck_s@yahoo.com>
2024-07-16 17:24:42 -0700

    Add an implementation of an in-memory book repository

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,6 +1,12 @@
 ﻿namespace Store;
 
-public class Class1
+public class Book
 {
-
+    public int Id { get; set; } 
+    public string Title { get; set; }
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
 }
